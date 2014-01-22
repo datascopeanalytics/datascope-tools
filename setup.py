04bd56d27487f0a32e032fb8c74d1e9a46e7433b
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2014-01-22 08:27:29 -0600

    copied initial setup script from dix

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='datascope_tools',
+    version='0.0.1',
+    description=(
+        'A suite of scripts, tools, and ideas that we use at Datascope'
+        'Analytics'
+    ),
+    url='https://github.com/datascopeanalytics/datascope-tools',
+    author='Datascope Analytics',
+    author_email='github.admin@datascopeanalytics.com',
+    license='MIT',
+    scripts=[],
+    zip_safe=False
+)
