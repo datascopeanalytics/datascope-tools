de83cbbee7d3576cddca72c02fef4ae383f7a914
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2014-01-22 15:08:30 -0600

    got setup script to work

--- setup.py
+++ setup.py
@@ -1,16 +1,39 @@
+import glob
+import os
 from setuptools import setup
 
+import datascope_tools
+
+# get all of the scripts
+scripts = glob.glob("bin/*")
+
+# read in the description from README
+with open("README.md") as stream:
+    long_description = stream.read()
+
+github_url='https://github.com/datascopeanalytics/datascope-tools'
+
+# read in the dependencies from the virtualenv requirements file
+dependencies = []
+filename = "REQUIREMENTS"
+with open(filename, 'r') as stream:
+    for line in stream:
+        package = line.strip().split('#')[0]
+        if package:
+            dependencies.append(package)
+
 setup(
-    name='datascope_tools',
-    version='0.0.1',
-    description=(
-        'A suite of scripts, tools, and ideas that we use at Datascope'
-        'Analytics'
-    ),
-    url='https://github.com/datascopeanalytics/datascope-tools',
+    name=datascope_tools.__name__,
+    version=datascope_tools.VERSION,
+    description="import random.scripts, random.tools, random.ideas",
+    long_description=long_description,
+    url=github_url,
+    download_url="%s/archives/master" % github_url,
     author='Datascope Analytics',
     author_email='github.admin@datascopeanalytics.com',
     license='MIT',
-    scripts=[],
-    zip_safe=False
+    scripts=scripts,
+    packages=[datascope_tools.__name__],
+    install_requires=dependencies,
+    zip_safe=False,
 )
