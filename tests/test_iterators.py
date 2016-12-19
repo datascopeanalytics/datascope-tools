06ed2083cfd8e34b9e3ffd71d35e95d5d73a9954
stringertheory <mike.stringer@datascopeanalytics.com>
2016-12-18 20:44:22 -0600

    added test of key function

--- tests/test_iterators.py
+++ tests/test_iterators.py
@@ -31,3 +31,42 @@ def test_rank_sorted():
     # now put in some ties
     bunga = [95, 95, 90, 85]
     assert list(rank_sorted(bunga)) == [(1, 95), (1, 95), (3, 90), (4, 85)]
+
+
+def test_rank_sorted_with_key():
+
+    import operator
+
+    scores = [
+        ('alex', 100),
+        ('bo', 100),
+        ('brian', 100),
+        ('dean', 10),
+        ('horatio', 90),
+        ('irmak', 100),
+        ('jess', 100),
+        ('karlota', 90),
+        ('melba', 90),
+        ('meridith', 100),
+        ('michael', 100),
+        ('mike', 100),
+        ('mollie', 100),
+        ('vlad', 100),
+    ]
+    answer = [
+        (1, ('alex', 100)),
+        (1, ('bo', 100)),
+        (1, ('brian', 100)),
+        (1, ('irmak', 100)),
+        (1, ('jess', 100)),
+        (1, ('meridith', 100)),
+        (1, ('michael', 100)),
+        (1, ('mike', 100)),
+        (1, ('mollie', 100)),
+        (1, ('vlad', 100)),
+        (11, ('horatio', 90)),
+        (11, ('karlota', 90)),
+        (11, ('melba', 90)),
+        (14, ('dean', 10)),
+    ]
+    assert list(rank_sorted(scores, key=operator.itemgetter(1))) == answer
